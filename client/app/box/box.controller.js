dd06cae9edd5aef5a3bb42772696377b0a4f1f1d
jalonjs <824525504@qq.com>
2015-03-30 13:43:31 +0800

    增加命令

--- client/app/box/box.controller.js
+++ client/app/box/box.controller.js
@@ -6,7 +6,7 @@ angular.module('endApp')
       //  更改title
       document.title = 'End盒子——用手机做你的随身手柄';
 
-      var myHost = '172.20.10.2:1643';
+      var myHost = 'http://192.168.1.106:1643';
       //  二维码， app
       $scope.maskShow = true;  //  显示遮罩及二维码
 
@@ -17,8 +17,9 @@ angular.module('endApp')
 
       //  通过id再次拿到这个app对应的地址，放到iframe里，以及获得她对应的二维码地址，生成二维码。
       storeAPI.getAppById(appId).success(function (app) {
-        $scope.appUrl = $sce.trustAsResourceUrl(app.appUrl);
-        $scope.qrcodeUrl = 'http://' + myHost + '/handle/' + uniqueId + '/type/' + (app.handleId || '1');
+        //$scope.appUrl = $sce.trustAsResourceUrl(app.appUrl);
+        $scope.appUrl = $sce.trustAsResourceUrl('http://localhost/enddemo');
+        $scope.qrcodeUrl = myHost + '/handle/' + uniqueId + '/type/' + (app.handleId || '1');
       });
 
       //  手柄加载成功后，去除二维码遮罩层
@@ -29,7 +30,10 @@ angular.module('endApp')
       //  命令
       socket.socket.on('box:' + uniqueId + ':cmd', function (cmd) {
         console.log(cmd);
-        //  todo
+        //  postMessage to frame
+        var iframeWin = document.getElementById("appPage").contentWindow;
+        iframeWin.postMessage(cmd, '*');
+
       });
 
   }]);
