72c21abfc4c4172e5b98416d79bc387ed46be5ec
jalonjs <824525504@qq.com>
2015-03-30 15:35:26 +0800

    增加命令

--- client/app/open/open.controller.js
+++ client/app/open/open.controller.js
@@ -2,5 +2,23 @@
 
 angular.module('endApp')
   .controller('OpenCtrl', function ($scope) {
-    $scope.message = 'Hello';
+
+    $scope.cmdList = [{
+      cmd:'up', handle:'上键 ↑'
+    },{
+      cmd:'down', handle:'下键 ↓'
+    },{
+      cmd:'left', handle:'左键 ←'
+    },{
+      cmd:'right', handle:'右键 →'
+    },{
+      cmd:'x', handle:'功能键 X'
+    },{
+      cmd:'y', handle:'功能键 Y'
+    },{
+      cmd:'z', handle:'功能键 Z'
+    }];
+
+
+
   });
